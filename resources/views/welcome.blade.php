395c83e123eee647d7817544ab4046e716557b91
mfzall07 <mfaisalhafid@gmail.com>
2022-04-29 14:43:57 +0700

    tab menu for price done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="/css/app.css" rel="stylesheet">
 
+    <title>Document</title>
+
+    <link href="/css/app.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    
     @livewireStyles
 </head>
 <body>
@@ -73,7 +76,7 @@
     <!-- end::about -->
 
     <!-- begin:::course -->
-    <div class="bg-zinc-800 p-10">
+    <div class="bg-zinc-800 p-10 ">
         <div>
             <h1 class="text-white text-2xl font-bold text-center">Course</h1>
             <h1 class="text-white text-sm text-center mt-5">
@@ -159,8 +162,195 @@
                 </div>
             </div>
         </div>
+        <div class="bg-black px-3 py-5 rounded-lg">
+            <h1 class="text-white list-disc ml-5">
+                <span><i class="las la-check pr-3"></i></span>
+                We offer you a practical knowledge with solid scientific background, and deliver to you in simple Indonesian language in three (3) days.
+            </h1>
+        </div>
+        <div class="bg-black px-3 py-5 rounded-lg mt-5">
+            <h1 class="text-white list-disc ml-5">
+                <span><i class="las la-check pr-3"></i></span>
+                We are not world's best, but we give you our best knowledge to help you increase competencies in order to help you increase your income up to 10 times.
+            </h1>
+        </div>
     </div>
     <!-- end:::course -->
+
+    <!-- begin::warning -->
+    <div>
+        <div class="bg-red-600">
+            <h1 class="text-white text-xl text-center font-semibold py-5">!!! Seats are limited to 30 persons so, stop waiting and start <a href="#" class="underline font-bold">register</a> now !!!</h1>
+        </div>
+    </div>
+    <!-- end::warning -->
+
+    <!-- begin::price -->
+    <div class="bg-gradient-to-b from-zinc-800 via-zinc-400 to-zinc-800 p-10 ">
+        <div>
+            <h1 class="text-center text-7xl font-bold text-white">Are You Ready ?</h1>
+            <h1 class="text-center text-lg text-white opacity-40 p-5">FITNESS PLUS INDONESIA will be held through three stages</h1>
+        </div>
+        <div class="py-5">
+            <div>
+                <ul class="flex flex-wrap -mb-px text-sm font-medium text-center text-white dark:text-gray-400" id="tabs">
+                    <li class="mr-2">
+                        <a href="#academy-one" class="inline-flex p-4 rounded-t-lg border-b-2 border-transparent hover:text-blue-400 hover:border-blue-400 group" id="default-tab">
+                            <i class="las la-dumbbell self-center pr-2"></i>Academy Level 1
+                        </a>
+                    </li>
+                    <li class="mr-2">
+                        <a href="#academy-two" class="inline-flex p-4 rounded-t-lg border-b-2 border-transparent hover:text-blue-400 hover:border-blue-400 group">
+                            <i class="las la-dumbbell self-center pr-2"></i>Academy Level 2
+                        </a>
+                    </li>
+                    <li class="mr-2">
+                        <a href="#academy-third" class="inline-flex p-4 rounded-t-lg border-b-2 border-transparent hover:text-blue-400 hover:border-blue-400 group">
+                            <i class="las la-dumbbell self-center pr-2"></i>Academy Level 3
+                        </a>
+                    </li>
+                </ul>
+                <div id="tab-contents">
+                    <div id="academy-one">
+                        <div class="bg-black p-10 mt-5 rounded-tr-xl rounded-tl-xl">
+                            <div>
+                                <h1 class="text-white font-bold text-xl">Level I</h1>
+                                <ul class="list-disc text-white p-5 grid grid-cols-4 gap-5">
+                                    <li>Praktek</li>
+                                    <li>Digital Marketing</li>
+                                    <li>Prepare Competition</li>
+                                    <li>Design Program Latihan</li>
+                                    <li>Biohecanical</li>
+                                    <li>Sales dan Marketing</li>
+                                    <li>Nutrisi</li>
+                                    <li>Anatomi</li>
+                                </ul>
+                            </div>
+                            <div class="py-5">
+                                <h1 class="text-white">Narasumber :</h1>
+                                <ol class="list-decimal text-white px-5 py-2">
+                                    <li>dr Tanjung Subrata, MRepro, ABAARM</li>
+                                    <li>dr. Phaidon Toruan MM</li>
+                                    <li>DR. Hariadin Mahardika S.E, M.M.</li>
+                                    <li>Nicholas yong Lee ( Muscle Mania Word champion)</li>
+                                    <li>Dith satyawan ( fitnesspreneur)</li>
+                                </ol>
+                            </div>
+                        </div>
+                        <div class="bg-zinc-900 p-10">
+                            <div>
+                                <h1 class="text-white opacity-25">23 - 25 Juni 2022</h1>
+                                <h1 class="text-white opacity-25 py-5">OFFLINE MODE / TATAP MUKA</h1>
+                                <h1 class="text-white opacity-25">Fitness Plus Marlboro : Jl. Mahendrata Selatan no 199</h1>
+                            </div>
+                            <div class="flex flex-row py-5">
+                                <div class="flex flex-col">
+                                    <h1 class="text-white opacity-25">Early bird price</h1>
+                                    <h1 class="text-yellow-300 text-2xl font-bold">Rp 2.999.000</h1>
+                                </div>
+                                <div class="flex flex-col ml-10">
+                                    <h1 class="text-white opacity-25">Regular price</h1>
+                                    <h1 class="text-yellow-300 text-2xl font-bold">Rp 3.499.000</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="academy-two" class="hidden">
+                        <div class="bg-black p-10 mt-5 rounded-tr-xl rounded-tl-xl">
+                            <div>
+                                <h1 class="text-white font-bold text-xl">Level II</h1>
+                                <ul class="list-disc text-white p-5 grid grid-cols-4 gap-5">
+                                    <li>Praktek</li>
+                                    <li>Digital Marketing</li>
+                                    <li>Prepare Competition</li>
+                                    <li>Design Program Latihan</li>
+                                    <li>Biohecanical</li>
+                                    <li>Sales dan Marketing</li>
+                                    <li>Nutrisi</li>
+                                    <li>Anatomi</li>
+                                </ul>
+                            </div>
+                            <div class="py-5">
+                                <h1 class="text-white">Narasumber :</h1>
+                                <ol class="list-decimal text-white px-5 py-2">
+                                    <li>dr Tanjung Subrata, MRepro, ABAARM</li>
+                                    <li>dr. Phaidon Toruan MM</li>
+                                    <li>DR. Hariadin Mahardika S.E, M.M.</li>
+                                    <li>Nicholas yong Lee ( Muscle Mania Word champion)</li>
+                                    <li>Dith satyawan ( fitnesspreneur)</li>
+                                </ol>
+                            </div>
+                        </div>
+                        <div class="bg-zinc-900 p-10">
+                            <div>
+                                <h1 class="text-white opacity-25">23 - 25 Juni 2022</h1>
+                                <h1 class="text-white opacity-25 py-5">OFFLINE MODE / TATAP MUKA</h1>
+                                <h1 class="text-white opacity-25">Fitness Plus Marlboro : Jl. Mahendrata Selatan no 199</h1>
+                            </div>
+                            <div class="flex flex-row py-5">
+                                <div class="flex flex-col">
+                                    <h1 class="text-white opacity-25">Early bird price</h1>
+                                    <h1 class="text-yellow-300 text-2xl font-bold">Rp 2.999.000</h1>
+                                </div>
+                                <div class="flex flex-col ml-10">
+                                    <h1 class="text-white opacity-25">Regular price</h1>
+                                    <h1 class="text-yellow-300 text-2xl font-bold">Rp 3.499.000</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="academy-third" class="hidden">
+                        <div class="bg-black p-10 mt-5 rounded-tr-xl rounded-tl-xl">
+                            <div>
+                                <h1 class="text-white font-bold text-xl">Level III</h1>
+                                <ul class="list-disc text-white p-5 grid grid-cols-4 gap-5">
+                                    <li>Praktek</li>
+                                    <li>Digital Marketing</li>
+                                    <li>Prepare Competition</li>
+                                    <li>Design Program Latihan</li>
+                                    <li>Biohecanical</li>
+                                    <li>Sales dan Marketing</li>
+                                    <li>Nutrisi</li>
+                                    <li>Anatomi</li>
+                                </ul>
+                            </div>
+                            <div class="py-5">
+                                <h1 class="text-white">Narasumber :</h1>
+                                <ol class="list-decimal text-white px-5 py-2">
+                                    <li>dr Tanjung Subrata, MRepro, ABAARM</li>
+                                    <li>dr. Phaidon Toruan MM</li>
+                                    <li>DR. Hariadin Mahardika S.E, M.M.</li>
+                                    <li>Nicholas yong Lee ( Muscle Mania Word champion)</li>
+                                    <li>Dith satyawan ( fitnesspreneur)</li>
+                                </ol>
+                            </div>
+                        </div>
+                        <div class="bg-zinc-900 p-10">
+                            <div>
+                                <h1 class="text-white opacity-25">23 - 25 Juni 2022</h1>
+                                <h1 class="text-white opacity-25 py-5">OFFLINE MODE / TATAP MUKA</h1>
+                                <h1 class="text-white opacity-25">Fitness Plus Marlboro : Jl. Mahendrata Selatan no 199</h1>
+                            </div>
+                            <div class="flex flex-row py-5">
+                                <div class="flex flex-col">
+                                    <h1 class="text-white opacity-25">Early bird price</h1>
+                                    <h1 class="text-yellow-300 text-2xl font-bold">Rp 2.999.000</h1>
+                                </div>
+                                <div class="flex flex-col ml-10">
+                                    <h1 class="text-white opacity-25">Regular price</h1>
+                                    <h1 class="text-yellow-300 text-2xl font-bold">Rp 3.499.000</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div>
+
+    </div>
+    <!-- end::price -->
 
 
 
@@ -169,5 +359,33 @@
     @livewireScripts
     <!-- End::Livewire -->
     <script src="https://unpkg.com/flowbite@1.4.2/dist/flowbite.js"></script>
+    <script>
+        let tabsContainer = document.querySelector("#tabs");
+
+        let tabTogglers = tabsContainer.querySelectorAll("a");
+        console.log(tabTogglers);
+
+        tabTogglers.forEach(function(toggler) {
+        toggler.addEventListener("click", function(e) {
+            e.preventDefault();
+
+            let tabName = this.getAttribute("href");
+
+            let tabContents = document.querySelector("#tab-contents");
+
+            for (let i = 0; i < tabContents.children.length; i++) {
+
+            tabTogglers[i].parentElement.classList.remove("border-blue-400", "border-b",  "-mb-px", "opacity-100");  tabContents.children[i].classList.remove("hidden");
+            if ("#" + tabContents.children[i].id === tabName) {
+                continue;
+            }
+            tabContents.children[i].classList.add("hidden");
+
+            }
+            e.target.parentElement.classList.add("border-blue-400", "border-b-4", "-mb-px", "opacity-100");
+        });
+        });
+        document.getElementById("default-tab").click();
+    </script>
 </body>
 </html>
