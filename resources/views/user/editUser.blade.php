73e051ccfd8b690e6cab217d9d4786ca83be4b81
mfzall07 <mfaisalhafid@gmail.com>
2022-05-10 14:58:38 +0700

    responsive editProfile

--- resources/views/user/editUser.blade.php
+++ resources/views/user/editUser.blade.php
@@ -70,7 +70,7 @@
                         <div class="mb-6">
                             <textarea type="text" id="address" class="bg-gray-50 rounded-lg border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Address" required></textarea>
                         </div>
-                        <div class="flex flex-row justify-end gap-4">
+                        <div class="flex flex-row justify-center md:justify-end gap-4">
                             <a href="/userProfile" class="text-white rounded-lg bg-transparent hover:bg-gray-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm px-5 py-2.5 text-center">Cancel</a>
                             <a href="{{ route('registerSuccess') }}" class="text-white rounded-lg bg-yellow-400 hover:bg-yellow-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm px-5 py-2.5 text-center">Save Changes</a>
                         </div>
