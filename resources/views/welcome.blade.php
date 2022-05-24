38af13e22f5d31a8299de7f77da1e58af0c31df7
Wiwitfar <96903072+Wiwitfar@users.noreply.github.com>
2022-05-24 13:54:32 +0700

    REVISI TEXT LANDING PAGE

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
     @livewireStyles
 </head>
 <body>
-    <livewire:lp.navbar /> 
+    <livewire:lp.navbar />
 
     <div id="home">
         <!-- begin::header -->
@@ -23,7 +23,7 @@
                 <div class="flex flex-col-reverse md:flex-row">
                     <div class="py-10 text-center md:text-left md:basis-3/6 md:self-center">
                         <h1 class="text-white text-3xl font-bold md:text-4xl">Welcome to Fitness Plus Academy</h1>
-                        <h1 class="text-sm text-gray-300 py-10">We serve you with knowledge to help you increase your skill, competencies to give results to your clients and tobhelp you increase your sales and marketing knowledge to increse your income.</h1>
+                        <h1 class="text-sm text-gray-300 py-10">We support you with the knowledge to improve your skills and abilities, deliver results to your customers, and  help you expand your sales and marketing knowledge to increase your income.</h1>
                         <a href="#" class="bg-yellow-300 px-10 py-3 rounded-md hover:bg-yellow-400 hover:text-white">View Course</a>
                     </div>
                     <div class="md:basis-3/6 flex justify-center md:justify-end">
@@ -33,7 +33,7 @@
             </div>
         </div>
         <!-- end::header -->
-    
+
         <!-- begin:about -->
         <div class="bg-zinc-800 py-10" id="about">
             <div class="md:px-20 px-10">
@@ -43,18 +43,18 @@
                         <h1 class="font-bold">VISI</h1>
                     </div>
                     <div class="bg-black px-3 py-5 rounded-bl-lg rounded-br-lg">
-                        <h1 class="text-white">Menjadikan pelatih kebugaran menjadi bagian dari profesi kesehatan yang diakui dan setara dgn profesi kesehatan lainnya, melalui pendidikan dan pelatihan yang terstruktur berdasarkan keilmuan dan pengetahuan ilmiah.</h1>
+                        <h1 class="text-white">Melalui pelatihan terstruktur dan pendidikan lebih lanjut berdasarkan pengetahuan ilmiah, menjadikan pelatih kebugaran sebagai profesi kesehatan yang diakui dan sejajar dengan profesi kesehatan lainnya.</h1>
                     </div>
                 </div>
-        
+
                 <div class="flex flex-col-reverse md:flex-row">
                     <div class="basis-3/6 py-5 md:py-0 md:pr-10" data-aos="fade-zoom-in" data-aos-offset="300" data-aos-easing="ease-in-sine">
                         <h1 class="text-white text-justify">
-                            Melihat semakin besarnya potensi pasar klub kebugaran sehingga meningkatkan kebutuhan akan klub dan pelatih yang berkualitas. Kenyataannya keberadaan pelatihan sertifikasi masih belum banyak, baik dalam kuantitas dan atau kualitas khususnya yang berhubungan dengan ilmu yang ilmiah tapi mudah dimengerti dan dipraktekkan, serta waktu pelatihan yang terbatas. 
+                            Seiring pertumbuhan pasar klub kebugaran dan semakin menjanjikan, kebutuhan akan klub dan pelatih berkualitas meningkat. Program pendidikan dan pelatihan yang terakreditasi masih kurang memadai dari segi kuantitas dan kualitas, terutama yang berkaitan dengan ilmu dan praktik yang mudah  dipahami  serta waktu pelatihan yang terbatas.
                         </h1>
                         <br>
                         <h1 class="text-white text-justify">
-                            Fitness plus academy ingin memberikan solusi pada anda yang sangat tertarik untuk masuk dalam industri Fitness, berupa materi baik yang mendasar maupun yang baru,  yang sesuai dengan trend kelinian. Fitness plus academy menganalisa, memilah-milah dan merangkum  kebutuhan - kebutuhan materi yang berkembangan pesat, dan  memformulasikan dalam sebuah kemasan pelatihan kekinian yang berkualitas dan efisien serta akan membantu memberikan hasil kepuasan pelanggan sehingga meningkatkan pendapatan. 
+                            Fitness plus Academy bertujuan untuk memberikan solusi bagi siapa saja yang ingin memasuki industri kebugaran, baik dalam  materi dasar maupun baru yang sejalan dengan tren saat ini. Fitness plus Academy menganalisis, mengkategorikan, dan merangkum kebutuhan  materi yang berkembang pesat ke dalam paket pelatihan kualitatif dan efisien terbaru untuk mencapai hasil kepuasan pelanggan dan meningkatkan penjualan.
                         </h1>
                     </div>
                     <div class="basis-3/6" data-aos="fade-zoom-in" data-aos-offset="300" data-aos-easing="ease-in-sine">
@@ -64,29 +64,29 @@
                 <div class="flex flex-col-reverse md:flex-row mt-10">
                     <div class="basis-3/6 py-5 md:py-0 md:pr-10" data-aos="fade-zoom-in" data-aos-offset="300" data-aos-easing="ease-in-sine">
                         <h1 class="text-white text-justify">
-                            Fitness Plus academy ingin agar masyarakat fitness atau pelatih kebugaran bisa mengikuti  formulasi  tersebut dengan efisien waktu yang tepat, padat berisi, dan anda akan sangat beruntung karena para pengajar yang membagikan ilmu dalam pelatihan ini memiliki kompetensi tinggi serta pengalaman di industri fitness lebih dari 10 tahun. 
+                            Fitness Plus Academy ingin komunitas kebugaran atau pelatih kebugaran dapat mengikuti formulasi mereka dengan konten yang efisien, tepat waktu, dan kaya. Pelatih yang menyampaikan ilmu dalam pelatihan ini memiliki keterampilan dan pengalaman  tinggi lebih dari 10 tahun di industri kebugaran, sehingga merupakan keuntungan untuk berpartisipasi di Akademi Fitness Plus.
                         </h1>
                         <br>
                         <h1 class="text-white text-justify">
-                        Fitness Plus Academy akan terus mengembangkan dunia pelatihan menjadi lebih luas, lebih terstruktur dan kedepan bisa memiliki pelatihan setara diploma 1 dan seterusnya. Sehingga lulusannya memiliki kemampuan yang lebih dan bersaing  dengan kebutuhan ilmu yang di butuhkan saat ini dan yang akan datang , dan memiliki kemampuan marketing, sales, management yang tidak dimiliki pelatihan lain. Fitness Plus Academy mendesain pelatihan yang berisikan ilmu utama berupa ilmu dasar kedokteran yakni anatomi,  fisiologi dan nutrisi, serta aplikasinya dalam bentuk ilmu biomekanik, hormon, dan suplementasi. Implementasi program Fitness Plus Academy bisa diterapkan untuk pelatihan personal training, program kompetisi, dan pelatihan untuk olahraga lain. 
+                            Fitness Plus Academy akan terus berkembang untuk menjadikan dunia pelatihan  lebih luas dan terstruktur, dan kedepannya kami berencana untuk memberikan pelatihan yang setara dengan Diploma 1 dan lainnya. Memungkinkan lulusan untuk memiliki  lebih banyak keterampilan, bersaing dengan persyaratan pengetahuan saat ini dan masa depan, dan memperoleh keterampilan pemasaran, penjualan, dan manajemen yang tidak ditemukan dalam kursus pelatihan lain. Fitness Plus Academy merancang kursus pelatihan yang mencakup pengetahuan kunci dalam bentuk pengetahuan medis dasar: anatomi, fisiologi, nutrisi, dan aplikasi dalam bentuk biomekanik, hormon, dan suplemen makanan. Pelaksanaan program Fitness Plus Academy dapat diterapkan pada pelatihan pribadi, program kompetisi, dan pelatihan  olahraga lainnya.
                         </h1>
                     </div>
                     <div class="basis-3/6" data-aos="fade-zoom-in" data-aos-offset="300" data-aos-easing="ease-in-sine">
                         <img src="assets/img/About/imageTwo.png" class="w-full h-96 rounded" alt="">
                     </div>
                 </div>
-                <h1 class="text-white text-justify mt-5">Fitness Plus  Academy di gawangi oleh pengajar2 yang memiliki kompetensi tinggi seperti dr. Tandjung, dr. Phaidon, Dr. Hariadin mahardika, atlit  international Nicholas yang, dan fitnesspreneur Dith satyawan.</h1>
+                <h1 class="text-white text-justify mt-5">Fitness Plus Academy dinaungi oleh pengajar-pengajar yang memiliki kompetensi tinggi seperti Dr. Tandjung, Dr. Phaidon, Dr. Harryadin Mahardika, atlet internasional Nicholas Yang, dan fitness preneur Dith satyawan.</h1>
             </div>
         </div>
         <!-- end::about -->
-    
+
         <!-- begin:::course -->
         <div class="bg-zinc-800 py-10" id="course">
             <div class="md:px-20 px-10" data-aos="fade-zoom-in" data-aos-offset="300" data-aos-easing="ease-in-sine">
                 <div>
                     <h1 class="text-white text-2xl font-bold text-center">Course</h1>
                     <h1 class="text-white text-sm text-center mt-5">
-                        We serve you with knowledge to help you increase your skill, competencies to give results to your clients and tobhelp you increase your sales and marketing knowledge to increse your income 
+                        We support you with the knowledge to improve your skills and abilities, deliver results to your customers, and  help you expand your sales and marketing knowledge to increase your income.
                     </h1>
                 </div>
                 <div class="grid grid-rows-1 md:grid-cols-3 gap-3 py-10">
@@ -97,12 +97,12 @@
                         <div class="bg-black px-3 py-5 rounded-bl-lg rounded-br-lg h-40">
                             <ul class="text-white list-disc ml-5">
                                 <li>Basic Anatomy</li>
-                                <li>Basic Physiology</li> 
-                                <li>Basic Nutrition</li> 
+                                <li>Basic Physiology</li>
+                                <li>Basic Nutrition</li>
                             </ul>
                         </div>
                     </div>
-        
+
                     <div>
                         <div class="bg-yellow-300 p-3 rounded-tl-lg rounded-tr-lg">
                             <h1 class="font-bold text-center">Level II (Online)</h1>
@@ -110,12 +110,12 @@
                         <div class="bg-black px-3 py-5 rounded-bl-lg rounded-br-lg h-40">
                             <ul class="text-white list-disc ml-5">
                                 <li>Anatomy and Biomechanics</li>
-                                <li>Resistance Training Programs</li> 
-                                <li>Intermediate Nutrition</li> 
+                                <li>Resistance Training Programs</li>
+                                <li>Intermediate Nutrition</li>
                             </ul>
                         </div>
                     </div>
-        
+
                     <div>
                         <div class="bg-yellow-300 p-3 rounded-tl-lg rounded-tr-lg">
                             <h1 class="font-bold text-center">Level III (Ofline)</h1>
@@ -123,12 +123,12 @@
                         <div class="bg-black px-3 py-5 rounded-bl-lg rounded-br-lg h-40">
                             <ul class="text-white list-disc ml-5">
                                 <li>Resistance Training Practice</li>
-                                <li>Nutrition Practice and Medical Fitness Testing</li> 
+                                <li>Nutrition Practice and Medical Fitness Testing</li>
                                 <li>Personal Training Management</li>
                             </ul>
                         </div>
                     </div>
-        
+
                     <div>
                         <div class="bg-yellow-300 p-3 rounded-tl-lg rounded-tr-lg">
                             <h1 class="font-bold text-center">Level IV (Online)</h1>
@@ -136,12 +136,12 @@
                         <div class="bg-black px-3 py-5 rounded-bl-lg rounded-br-lg h-40">
                             <ul class="text-white list-disc ml-5">
                                 <li>Exercise and Endocrine Systems</li>
-                                <li>Exercise Medicine</li> 
-                                <li>Advance Nutrition</li> 
+                                <li>Exercise Medicine</li>
+                                <li>Advance Nutrition</li>
                             </ul>
                         </div>
                     </div>
-        
+
                     <div>
                         <div class="bg-yellow-300 p-3 rounded-tl-lg rounded-tr-lg">
                             <h1 class="font-bold text-center">Level V (Online)</h1>
@@ -149,12 +149,12 @@
                         <div class="bg-black px-3 py-5 rounded-bl-lg rounded-br-lg h-40">
                             <ul class="text-white list-disc ml-5">
                                 <li>Aerobic Training and Cardiorespiratory Endurance</li>
-                                <li>Stretching and Flexibility</li> 
-                                <li>Functional Training</li> 
+                                <li>Stretching and Flexibility</li>
+                                <li>Functional Training</li>
                             </ul>
                         </div>
                     </div>
-        
+
                     <div>
                         <div class="bg-yellow-300 p-3 rounded-tl-lg rounded-tr-lg">
                             <h1 class="font-bold text-center">Level VI (Online)</h1>
@@ -162,8 +162,8 @@
                         <div class="bg-black px-3 py-5 rounded-bl-lg rounded-br-lg h-40">
                             <ul class="text-white list-disc ml-5">
                                 <li>Basic Life Support and First Aid Training (additional certificate)</li>
-                                <li>Functional Training Practice</li> 
-                                <li>Motivation, Businesses Management and Fitnesspreneur</li> 
+                                <li>Functional Training Practice</li>
+                                <li>Motivation, Businesses Management and Fitnesspreneur</li>
                             </ul>
                         </div>
                     </div>
@@ -183,7 +183,7 @@
             </div>
         </div>
         <!-- end:::course -->
-    
+
         <!-- begin::warning -->
         <div>
             <div class="bg-red-600">
@@ -191,7 +191,7 @@
             </div>
         </div>
         <!-- end::warning -->
-    
+
         <!-- begin::price -->
         <div class="bg-gradient-to-b from-zinc-800 via-zinc-400 to-zinc-800 py-10">
             <div class="md:px-20 px-10" data-aos="fade-zoom-in" data-aos-offset="300" data-aos-easing="ease-in-sine">
@@ -329,7 +329,7 @@
             </div>
         </div>
         <!-- end::price -->
-    
+
         <!-- begin::coaches -->
         <div class="bg-zinc-800 py-10" id="coaches">
             <div class="md:px-20 px-10" data-aos="fade-zoom-in" data-aos-offset="300" data-aos-easing="ease-in-sine">
@@ -341,7 +341,7 @@
             </div>
         </div>
         <!-- end::coaches -->
-    
+
         <!-- begin::gallery -->
         <div class="bg-zinc-800 py-10" id="gallery">
             <div class="md:px-20 px-10" data-aos="fade-zoom-in" data-aos-offset="300" data-aos-easing="ease-in-sine">
