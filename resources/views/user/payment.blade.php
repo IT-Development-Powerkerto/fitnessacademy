98cae5c5faa2af37d667d1245f9b1af613f3c75b
Wiwitfar <96903072+Wiwitfar@users.noreply.github.com>
2022-05-25 14:23:20 +0700

    add blur modal payment

--- resources/views/user/payment.blade.php
+++ resources/views/user/payment.blade.php
@@ -45,7 +45,7 @@
                     See Full Image
                 </button>
                 <!-- Main modal -->
-                <div id="imageFull" tabindex="-1" aria-hidden="true" class="bg-black bg-opacity-75 hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
+                <div id="imageFull" tabindex="-1" aria-hidden="true" class="backdrop-blur-md bg-black bg-opacity-75 hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
                    <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
                        <!-- Modal content -->
                        <div class="relative bg-white rounded-lg shadow">
@@ -158,7 +158,7 @@
                                     </button>
                                     {{-- begin::modal --}}
                                     <!-- Main modal -->
-                                    <div id="defaultModal" tabindex="-1" aria-hidden="true" class="bg-black bg-opacity-75 hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
+                                    <div id="defaultModal" tabindex="-1" aria-hidden="true" class="backdrop-blur-md bg-black bg-opacity-75 hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
                                         <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
                                             <!-- Modal content -->
                                             <div class="relative bg-white rounded-lg shadow">
