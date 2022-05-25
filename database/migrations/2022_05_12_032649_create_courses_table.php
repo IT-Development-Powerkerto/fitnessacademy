95772b824d40e12631ba6b3fa2a2b0c1efd1d0e0
Wiwitfar <96903072+Wiwitfar@users.noreply.github.com>
2022-05-25 08:26:28 +0700

    Revisi Foreginid trainer id Table course

--- database/migrations/2022_05_12_032649_create_courses_table.php
+++ database/migrations/2022_05_12_032649_create_courses_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->integer('trainer_id');
+            $table->foreignId('trainer_id')->constrained('users');
             $table->string('level');
             $table->string('schedule');
             $table->integer('price')->nullable();
