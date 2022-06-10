f69a7faccd6b0c5b33b9ee31b7012e494e518cb9
Wiwitfar <96903072+Wiwitfar@users.noreply.github.com>
2022-06-10 09:10:42 +0700

    REV VIEW DETAIL COURSE

--- resources/views/trainer/detailSession.blade.php
+++ resources/views/trainer/detailSession.blade.php
@@ -15,7 +15,7 @@
 
     <livewire:trainer.navbar-trainer />
 
-    <div class="bg-zinc-800 p-10">
+    <div class="bg-zinc-800 p-10 h-full">
         <div class="">
             <div class="flex flex-row py-2 items-center">
                 <a href="/dashboard" class="text-white text-sm hover:text-yellow-300">Dashboard</a>
